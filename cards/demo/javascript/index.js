6c3cd831e3a3cb9aca8ca49a27add347cb9819e7
Shane <shanecs@me.com>
2018-12-15 14:32:12 -0600

    Add files via upload

---
+++ cards/demo/javascript/index.js
@@ -0,0 +1,29 @@
+var Card = (function() {
+
+  // Variables
+	var card;
+
+
+  // Constructor (Set Variables)
+	function Card() {
+		// this.title = title;
+	}
+
+  // Prototypes
+	// Card.prototype.draw = function() {
+	// };
+
+  // Initialize Class
+	Card.init = function(container) {
+    this.card = new Card()
+	};
+
+	return Card;
+
+}());
+
+window.onload = function() {
+  setTimeout(function() {
+  	Card.init();
+  }, 500);
+}
